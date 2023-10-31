2fa84c079ad138b4ab14a26cb6b7354b0e0815de
MSrafli <tidakadaakun09@gmail.com>
2023-10-31 14:39:24 +0800

    firts commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,21 @@
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid">
+        <div class="d-flex align-items-center justify-contentbetween small">
+            <div class="textmuted">Copyright &copy; Your Website 2021</div>
+            <div>
+                <a href="#">Privacy Policy</a>
+                &middot;
+                <a href="#">Terms &amp; Conditions</a>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery3.5.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstr
+ap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url('assets/js/scripts.js') ?>"></script>
+</body>
+
+</html>
